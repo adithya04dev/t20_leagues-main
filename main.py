b35cecaa659a7a692fba2d4647c3b0ea9a682607
adithya04dev <adithyabalagoni11@gmail.com>
2023-08-21 22:14:29 +0530

    initial

---
+++ main.py
@@ -0,0 +1,33 @@
+
+
+import streamlit as st 
+
+
+st.title('T20 Leagues Stats Page')
+
+st.markdown("[Batter Stats(Individual)](https://t20leaguesbatters.streamlit.app/)")
+st.write('Analyse batters stats by phases,bowling types')
+st.markdown("[Bowler Stats(Individual)](https://t20leagues-bowlers.streamlit.app/)")
+st.write('Analyse batters stats by phases,bowling types')
+st.markdown("[Batters Comparison](https://t20leagues-tbowling-comp.streamlit.app/)")
+st.write('Compare batters by phases,bowling types')
+st.markdown("[Bowlers Comparison](https://t20leagues-bowlerscomp.streamlit.app/)")
+st.write('Compare bowlers by phases,batting types')
+st.markdown("[Teams Batting Comparison](https://t20leagues-tbatting.streamlit.app/)")
+st.write('Compare Teams Batting by phases,bowling types')
+st.markdown("[Teams Bowling Comparison](https://t20leagues-tbowling.streamlit.app/)")
+st.write('Compare Teams Bowling by phases,batting types')
+st.markdown("[Teams Batting Stats(Individual)](https://t20leagues-tbatting-comp.streamlit.app/)")
+st.write('Analyse Teams Batting stats by phases,bowling types')
+st.markdown("[Teams Bowling (Individual)](https://t20leagues-teamsbowling-comp.streamlit.app/)")
+st.write('Analyse Teams Bowling  stats by phases,bowling types')
+
+
+
+
+
+
+st.write("Hope it helps you")
+
+
+
